9cd8155b0d278c54129195579ef089324139eafe
Walter Schweitzer <ec2-user@ip-10-212-121-158.ec2.internal>
2012-05-29 23:35:21 +0000

    added config

---
+++ app/Config/buildtables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE custmoerkeys (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    customer VARCHAR(50),
+    accesskey VARCHAR(50),
+    products VARCHAR(20),
+    expires DATETIME DEFAULT NULL,
+    notes TEXT 
+);
+
+CREATE TABLE solutions (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    products VARCHAR(50),
+    category VARCHAR(30),
+    demo_url VARCHAR(50),
+    slide_name VARCHAR(250),
+    slide_type VARCHAR(15),
+    video_name VARCHAR(250),
+    video_type VARCHAR(15),
+    notes TEXT
+);
